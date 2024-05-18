66d644d0c2716870a923f4131a4a778c31bdf92e
Rajat <rajatsaxena6397@gmail.com>
2024-05-18 23:46:51 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+   <div>
+    <h1>Image Generation App</h1>
+    <input type="text" placeholder='Enter text....'/>
+   </div>
+  )
+}
+
+export default App
